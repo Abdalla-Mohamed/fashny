5ad81c519f8fcea6b4efdb7a099f6dce709d27ac
Administrator <Administrator@DESKTOP-IJCM350>
2016-05-26 13:36:35 +0200

    place_View

--- src/java/com/iti/fashny/managedbeans/PlaceViewManagedBean.java
+++ src/java/com/iti/fashny/managedbeans/PlaceViewManagedBean.java
@@ -6,24 +6,36 @@
 package com.iti.fashny.managedbeans;
 
 import com.iti.fashny.entities.Place;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import java.util.Locale;
 import javax.faces.model.DataModel;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import org.primefaces.event.SelectEvent;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author Bakar M.M.R
  */
-@ManagedBean(name="placeView")
-@RequestScoped
+@ManagedBean(name = "placeView")
+@ViewScoped
 public class PlaceViewManagedBean {
 
     @ManagedProperty(value = "#{placeManagedBean}")
     private PlaceManagedBean placeManagedBean;
 
     DataModel<Place> filterPlaces;
+    private Place placeView;
+
+    public void setPlace(Place place) {
+        this.placeView = place;
+    }
+
+    public Place getPlace() {
+        return placeView;
+    }
 
     public DataModel<Place> getFilterPlaces() {
         return filterPlaces;
@@ -32,6 +44,7 @@ public class PlaceViewManagedBean {
     public void setFilterPlaces(DataModel<Place> filterPlaces) {
         this.filterPlaces = filterPlaces;
     }
+
     public PlaceManagedBean getPlaceManagedBean() {
         return placeManagedBean;
     }
@@ -39,21 +52,34 @@ public class PlaceViewManagedBean {
     public void setPlaceManagedBean(PlaceManagedBean placeManagedBean) {
         this.placeManagedBean = placeManagedBean;
     }
-    
+
     public PlaceViewManagedBean() {
     }
-    
+
     public boolean filterBy(Object value, Object filter, Locale locale) {
         String filterText = (filter == null) ? null : filter.toString().trim();
-        if(filterText == null||filterText.equals("")) {
+        if (filterText == null || filterText.equals("")) {
             return true;
         }
-         
-        if(value == null) {
+
+        if (value == null) {
             return false;
         }
-         
+
         return ((Comparable) value).compareTo(Integer.valueOf(filterText)) > 0;
     }
-    
+
+//    public Place getSelectedPlace() {
+//
+//        place = filterPlaces.getRowData();
+//        System.out.println("test DM");
+//        return place;
+//    }
+
+//    public void onPlaceChosen(SelectEvent event) {
+//         place = (Place) event.getObject();
+//        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Car Selected", "Id:" + place.getId());
+//
+//        FacesContext.getCurrentInstance().addMessage(null, message);
+//    }
 }
