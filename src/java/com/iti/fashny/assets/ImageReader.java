aaa8e255864e645919a4e96c8b4752b3d9a8f4e4
Abdalla <Abdalla@192.168.56.1>
2016-06-05 18:26:23 +0200

    image reader

---
+++ src/java/com/iti/fashny/assets/ImageReader.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iti.fashny.assets;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.awt.print.Book;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Abdalla
+ */
+@WebServlet(name = "ImageReader", urlPatterns = {"/Image"})
+public class ImageReader extends HttpServlet {
+ static public final String uplodedImgFolderDestntion = "C:/Book_Shop/images/";
+//../productImage?imageName=${bookdetials.BFrontImg}"   
+
+ /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+ /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("image/png");
+        String imageName = request.getParameter("imageName");
+        int thumbWidth = 350;
+        int thumbHeight = 460;
+//        String pathToWeb = "C:/Book_Shop/images/";//getServletContext().getRealPath("/");
+        File f = new File(imageName);
+        System.out.println(f.getAbsolutePath());
+        BufferedImage image = ImageIO.read(f);
+
+//        BufferedImage thumb = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+//        Graphics2D graphics2D = thumb.createGraphics();
+//        graphics2D.setBackground(Color.WHITE);
+//        graphics2D.setPaint(Color.WHITE);
+//        graphics2D.fillRect(0, 0, thumbWidth, thumbHeight);
+//        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//        graphics2D.drawImage(image, 0, 0, thumbWidth, thumbHeight, null);
+
+        OutputStream out = response.getOutputStream();
+        ImageIO.write(image, "png", out);
+        out.close();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
