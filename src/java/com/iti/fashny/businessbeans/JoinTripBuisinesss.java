a33d505cf516ec2e8a3dd14b2f9cfbbe5657da79
habdalnaser <habdalnaser@gmail.com>
2016-06-15 20:18:37 +0200

    view every trip rate

--- src/java/com/iti/fashny/businessbeans/JoinTripBuisinesss.java
+++ src/java/com/iti/fashny/businessbeans/JoinTripBuisinesss.java
@@ -7,7 +7,9 @@ package com.iti.fashny.businessbeans;
 
 import com.iti.fashny.daos.DaoFactory;
 import com.iti.fashny.daos.JoinTripFacade;
+import com.iti.fashny.daos.TripFacade;
 import com.iti.fashny.entities.JoinTrip;
+import com.iti.fashny.entities.Trip;
 import javax.persistence.criteria.Join;
 
 /**
@@ -17,10 +19,31 @@ import javax.persistence.criteria.Join;
 public class JoinTripBuisinesss {
     DaoFactory daoFactory = new DaoFactory();
     JoinTripFacade joinTripFacade = daoFactory.getJoinTripDoa();
-    
+    TripFacade tripFacade = daoFactory.getTripDoa();
     public void rate(JoinTrip joinTrip){
         daoFactory.beginTransaction();
         joinTripFacade.edit(joinTrip);
         daoFactory.commitTransaction();
     }
+    
+    public int tripRate(Trip trip){
+        
+     int rate =0;
+     int voters = 0;
+     trip = tripFacade.find(trip.getId());
+     
+        for(JoinTrip joinTrip :trip.getJoinTripList()){
+            if(joinTrip.getRate()>0){
+            rate+=joinTrip.getRate();
+            voters++;
+            }
+        }
+        
+        if(voters >0)
+            rate = rate/voters;
+            System.out.println(rate);
+            
+        return rate;
+    }
+    
 }
